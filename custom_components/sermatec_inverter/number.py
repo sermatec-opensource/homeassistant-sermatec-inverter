978fad04101592910fc1897ca3a2278bb4c4bd4f
Ondrej Golasowski <golasowski.o@gmail.com>
2024-07-16 14:16:58 +0200

    Cleaned up dependencies

--- custom_components/sermatec_inverter/number.py
+++ custom_components/sermatec_inverter/number.py
@@ -6,10 +6,7 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant, callback
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.update_coordinator import (
-    CoordinatorEntity,
-    DataUpdateCoordinator,
-    UpdateFailed,
-    ConfigEntryNotReady
+    CoordinatorEntity
 )
 from homeassistant.exceptions import HomeAssistantError
 
